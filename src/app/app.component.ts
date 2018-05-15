84f8f97f9ab8dc58961524d1aa04ee2a91052328
Nicolas Quartier <quartier.nicolas@gmail.com>
2018-05-15 21:38:01 +0200

    adding flickrservice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import  { MenuComponent} from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { SculptuurComponent } from './sculptuur/sculptuur.component';
+import {FlickrServiceService} from './flickr-service.service';
+import {GlobalsService} from './globals.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SculptuurComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FlickrServiceService, GlobalsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
