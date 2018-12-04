83955c53d08532db2ee507a3bb894a5362805ec0
Nicolas Quartier <quartier.nicolas@gmail.com>
2018-12-04 22:05:26 +0100

    add basic rest endpoint for flickr api methods

--- src/app/globals.service.ts
+++ src/app/globals.service.ts
@@ -10,6 +10,7 @@ export class GlobalsService {
   activePage = '';
   requestTokenBaseUrl = 'https%3A%2F%2Fwww.flickr.com%2Fservices%2Foauth%2Frequest_token';
   accessTokenBaseUrl = 'https%3A%2F%2Fwww.flickr.com%2Fservices%2Foauth%2Faccess_token';
+  basicRestRequestUrl = 'https%3A%2F%2Fapi.flickr.com%2Fservices%2Frest';
   hmacSigningSecret = '';
   oauthToken = '';
 }
